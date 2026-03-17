a1919f1d389649d659392b3c3369c6ef265e68b0
Quang Tuong <quantruong1104@gmail.com>
2026-03-17 22:39:50 +0700

    fix: serve media files in production using re_path+serve instead of static() helper

--- cellphone_store/urls.py
+++ cellphone_store/urls.py
@@ -15,19 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    # Always serve media files regardless of DEBUG (needed for Render production)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Serve media files
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
